ad602145429d65b4bcdc9cbe759c94b8dcbb5108
Federico <federico.agostinis@otlook.com>
2022-02-04 15:02:01 +0100

    right object

--- 09_10x_hvgSelection.R
+++ 09_10x_hvgSelection.R
@@ -7,9 +7,13 @@ dati <- TENxBrainData()
 
 dati <- computeLibraryFactors(dati)
 dati <- logNormCounts(dati)
+dec <- modelGeneVar(dati)
+hvg <- getTopHVGs(dec, n=1000)
 
-hvg <- getTopHVGs(dati, n=1000)
-
+names = dati@rowRanges@elementMetadata$Symbol
+hvg_names = names[hvg]
+hvg = names %in% hvg_names
+names(hvg) = names
 name <- paste0("10x_hvg.Rdata")
 
 save(hvg, file = name)
